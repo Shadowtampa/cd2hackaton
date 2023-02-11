05f7f0d38f5c5501939a53d16acf46e119cb524d
neyvo.souza <neyvo.souza@tjac.jus.br>
2023-02-10 21:23:53 -0500

    tela de mapa

--- cd2_front/src/pages/Dashboard/Dashboard.tsx
+++ cd2_front/src/pages/Dashboard/Dashboard.tsx
@@ -44,8 +44,11 @@ export const Dashboard = () => {
   const [cityState, setCityState] = useState("");
   const [cityFoundation, setCityFoundation] = useState("");
 
-  const [neighbourhoodName, setNeighbourhoodName] = useState("");
-  const [neighbourhoodCity, setNeighbourhoodCity] = useState("");
+  const [chartName, setMapName] = useState("");
+  const [chartCity, setMapCity] = useState("");
+
+  const [origem, setOrigem] = useState("");
+  const [destino, setDestino] = useState("");
 
 
   const auth = useSelector((state: RootState) => state.auth)
@@ -65,29 +68,29 @@ export const Dashboard = () => {
   const neighbourhoods = useSelector((state: RootState) => state.neighbourhood.neighbourhoods);
 
   const [presentationCities, setPresentationCities] = useState(cities)
-  const [presentationNeighbourhoods, setPresentationNeighbourhoods] = useState(neighbourhoods)
+  const [presentationMaps, setPresentationMaps] = useState(neighbourhoods)
 
   const dispatch = useDispatch<AppDispatch>()
 
-  const handleAddNeighbourHood = (neighbourhoodData: { name: string, city: string }) => {
-    const city_id = cities.filter(cityItem => cityItem.name === neighbourhoodData.city)[0].id;
-    instance.post('/neighbourhood',
-      { name: neighbourhoodData.name, city_id: city_id },
+  const handleAddNeighbourHood = (chartData: { name: string, city: string }) => {
+    const city_id = cities.filter(cityItem => cityItem.name === chartData.city)[0].id;
+    instance.post('/chart',
+      { name: chartData.name, city_id: city_id },
       {
         headers: { Authorization: `Bearer ${auth.token}` }
       })
       .then(function (response: any) {
         alert("registro inserido com sucesso!")
-        instance.get('/neighbourhood',
+        instance.get('/chart',
           {
             headers: { Authorization: `Bearer ${auth.token}` }
           })
           .then(function (response: any) {
-            let tempNeighbourhood: { id: any; name: any; city_id: any; }[] = []
+            let tempMap: { id: any; name: any; city_id: any; }[] = []
             response.data.map((item: { id: any; name: any; city_id: any }) => {
-              tempNeighbourhood.push({ id: item.id, name: item.name, city_id: item.city_id })
+              tempMap.push({ id: item.id, name: item.name, city_id: item.city_id })
             })
-            dispatch(setNeighbourhood(tempNeighbourhood))
+            dispatch(setNeighbourhood(tempMap))
           })
       })
 
@@ -131,16 +134,16 @@ export const Dashboard = () => {
         })
         dispatch(setCities(tempCities))
 
-        const responseNeighbourhood = instance.get('/neighbourhood',
+        const responseMap = instance.get('/chart',
           {
             headers: { Authorization: `Bearer ${auth.token}` }
           })
           .then(function (response: any) {
-            let tempNeighbourhood: { id: any; name: any; city_id: any; }[] = []
+            let tempMap: { id: any; name: any; city_id: any; }[] = []
             response.data.map((item: { id: any; name: any; city_id: any }) => {
-              tempNeighbourhood.push({ id: item.id, name: item.name, city_id: item.city_id })
+              tempMap.push({ id: item.id, name: item.name, city_id: item.city_id })
             })
-            dispatch(setNeighbourhood(tempNeighbourhood))
+            dispatch(setNeighbourhood(tempMap))
           })
 
       })
@@ -160,7 +163,7 @@ export const Dashboard = () => {
     setPresentationCities(cities)
   }, [cities])
   useEffect(() => {
-    setPresentationNeighbourhoods(neighbourhoods)
+    setPresentationMaps(neighbourhoods)
   }, [neighbourhoods])
 
   return (
@@ -169,144 +172,43 @@ export const Dashboard = () => {
 
         <span style={{ fontWeight: "bold" }}>Olá {auth.userName}!!</span>
         <Tabs
-          defaultActiveKey="city"
+          defaultActiveKey="chart"
           id="uncontrolled-tab-example"
           className="mb-3"
         >
-          <Tab eventKey="city" title="City">
-
-            <div className='main-table-header-wrapper'>
-              <div className='filter-wrapper'>
-                <div className='filter'>
-                  <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Nome da Cidade</Form.Label>
-                    <Form.Control type="email" placeholder="Qual cidade?" onChange={(event) => { setPresentationCities(cities.filter(city => city.name.toLocaleLowerCase().includes(event.target.value.toLocaleLowerCase()))) }} />
-                  </Form.Group>
-                </div>
-                <div className='filter'>
-                  <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Estado</Form.Label>
-                    <Form.Control type="email" placeholder="Qual Estado?" onChange={(event) => { setPresentationCities(cities.filter(city => city.state.toLocaleLowerCase().includes(event.target.value.toLocaleLowerCase()))) }} />
-                  </Form.Group>
-                </div>
-                <div className='filter'>
-                  <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Data da Fundação</Form.Label>
-                    <Form.Control type="date" placeholder="Data de fundação?" onChange={(event) => { setPresentationCities(cities.filter(city => city.foundation.toString().includes(event.target.value))) }} />
-                  </Form.Group>
-
-
-                </div>
-
-              </div>
-
-
-
-            </div>
-
-
-            <Table striped hover>
-              <thead>
-                <tr>
-                  <th>#</th>
-                  <th>Nome</th>
-                  <th>Estado</th>
-                  <th>Data Fundação</th>
-                </tr>
-              </thead>
-              <tbody>
-
-                {presentationCities !== undefined && presentationCities.map(city =>
-                  <tr key={city.id}>
-                    <td>{city.id}</td>
-                    <td>{city.name}</td>
-                    <td>{city.state}</td>
-                    <td>{city.foundation.toString()}</td>
-                  </tr>
-                )}
-              </tbody>
-            </Table>
-
-            <div className='add-wrapper'>
-              <Button onClick={() => openModal("city")}>Adicionar Cidade</Button>
-            </div>
-          </Tab>
-          <Tab eventKey="neighbourhood" title="Neighbourhood">
+          
+          <Tab eventKey="chart" title="Map">
 
             <div className='main-table-header-wrapper'>
 
               <div className='filter-wrapper'>
                 <div className='filter'>
                   <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Nome do Bairro</Form.Label>
-                    <Form.Control type="text" placeholder="Qual bairro?" onChange={(event) => {
-                      setPresentationNeighbourhoods(neighbourhoods.filter(
-                        neighbourhood => neighbourhood.name.toLocaleLowerCase().includes(event.target.value.toLocaleLowerCase())))
+                    <Form.Label>Origem</Form.Label>
+                    <Form.Control type="text"  onChange={(event) => {
+                      setOrigem(event.target.value)
                     }} />
                   </Form.Group>
                 </div>
                 <div className='filter'>
                   <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Nome da cidade</Form.Label>
-                    <Form.Control type="text" placeholder="Qual Cidade?" onChange={(event) => {
-
-                      setPresentationNeighbourhoods(
-                        /**
-                         * 1º Fetch all cities that contain the text ont he input
-                         * 2º Fetch all ids from the fetched cities
-                         * 3º Fetch all neighbours that contain any of the fetched IDs
-                         * */
-                        neighbourhoods.filter(neighbour => cities.filter(city => city.name.toLocaleLowerCase().includes(event.target.value.toLocaleLowerCase())).map((city) => city.id).includes(neighbour.city_id))
-
-                      )
+                    <Form.Label>Destino</Form.Label>
+                    <Form.Control type="text"  onChange={(event) => {
+                      setDestino(event.target.value)
                     }} />
                   </Form.Group>
                 </div>
-                <div className='filter'>
-                  <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Nome do Estado</Form.Label>
-                    <Form.Control type="text" placeholder="Qual Estado?" onChange={(event) => {
-
-                      setPresentationNeighbourhoods(
-                        /**
-                         * 1º Fetch all cities that contain te state property equal to the input value
-                         * 2º Fetch all ids from the fetched cities
-                         * 3º Fetch all neighbours that contain any of the fetched IDs
-                         * */
-                        neighbourhoods.filter(neighbour => cities.filter(city => city.state.toLocaleLowerCase().includes(event.target.value.toLocaleLowerCase())).map((city) => city.id).includes(neighbour.city_id))
-
-                      )
-                    }} />
-                  </Form.Group>
-                </div>
+                <div className='filter-search'>
+              <Button onClick={() => console.log({origem,destino}) } >Buscar</Button>
+            </div>
 
               </div>
             </div>
 
 
-            <Table striped hover>
-              <thead>
-                <tr>
-                  <th>#</th>
-                  <th>Nome</th>
-                  <th>Cidade</th>
-                </tr>
-              </thead>
-              <tbody>
+          
 
-                {presentationNeighbourhoods.map(neighbourhood =>
-                  <tr key={neighbourhood.id}>
-                    <td>{neighbourhood.id}</td>
-                    <td>{neighbourhood.name}</td>
-                    <td>{neighbourhood.name !== "Carregando..." ? cities.filter(city => city.id === neighbourhood.city_id)[0].name : null}</td>
-                  </tr>
-                )}
-              </tbody>
-            </Table>
-
-            <div className='add-wrapper'>
-              <Button onClick={() => openModal("neighbourhood")} >Adicionar Bairro</Button>
-            </div>
+           
 
           </Tab>
         </Tabs>
@@ -363,24 +265,24 @@ export const Dashboard = () => {
               <Button onClick={() => handleAddCity({name: cityName, foundation: cityFoundation, state :cityState})}>Adicionar Cidade</Button>
             </div>
           </Tab>
-          <Tab eventKey="neighbourhood" title="Neighbourhood">
+          <Tab eventKey="chart" title="Map">
 
             <div className='main-table-header-wrapper'>
 
               <div className='filter-wrapper'>
                 <div className='filter'>
                   <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Nome do Bairro</Form.Label>
-                    <Form.Control type="text" placeholder="Nome do novo bairro:" onChange={(event) => {
-                      setNeighbourhoodName(event.target.value)
+                    <Form.Label>Origem</Form.Label>
+                    <Form.Control type="text"  onChange={(event) => {
+                      setMapName(event.target.value)
                     }} />
                   </Form.Group>
                 </div>
                 <div className='filter'>
                   <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Nome da cidade</Form.Label>
-                    <Form.Control type="text" placeholder="Qual Cidade?" onChange={(event) => {
-                      setNeighbourhoodCity(event.target.value)
+                    <Form.Label>Destino</Form.Label>
+                    <Form.Control type="text"  onChange={(event) => {
+                      setMapCity(event.target.value)
                     }} />
                   </Form.Group>
                 </div>
@@ -390,7 +292,7 @@ export const Dashboard = () => {
 
 
             <div className='add-wrapper'>
-              <Button onClick={() => handleAddNeighbourHood({ name: neighbourhoodName, city: neighbourhoodCity })}>Adicionar Bairro</Button>
+              <Button onClick={() => handleAddNeighbourHood({ name: chartName, city: chartCity })}>Adicionar Bairro</Button>
             </div>
           </Tab>
         </Tabs>
